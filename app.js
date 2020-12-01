702a8338eca137d12f4b9d65d7515c9ee7ed8731
michaeljylam <michael.lam8@live.com>
2020-11-30 21:22:52 -0500

    Add messageDelete event listener and update existing listeners

--- app.js
+++ app.js
@@ -12,12 +12,12 @@ for (const file of commandFiles) {
   client.commands.set(command.name, command);
 }
 
-client.on('ready', () => {
+client.once('ready', () => {
   console.log('Woof!');
-  client.user.setActivity("with an ultimate boomerang", { type: "PLAYING" })
+  client.user.setActivity("with a ball of yarn", { type: "PLAYING" })
 });
 
-client.on("message", message => {
+client.on('message', message => {
   const msg = message.content.toLowerCase();
   const swearingEnabled = require('./commands/noswearing.js');
   const grammarCheck = require('./commands/data/grammarcheck.js');
@@ -97,12 +97,12 @@ client.on("message", message => {
 
   for (let i = 0; i < grammarCheck.length; i++) {
     if (msg.indexOf(grammarCheck[i].grammarError) !== -1) {
-      try {
-        message.react("🐶")
-        message.reactions.removeAll()
-      } catch(error) {
-        message.member.setNickname(`Woof woof! (It's ${grammarCheck[i].correction}!)`).catch(console.error);
-      }
+      message.react("🐶")
+        .then(() => message.reactions.removeAll())
+        .catch(error => {
+          console.error(error)
+          message.member.setNickname(`Woof woof! (It's ${grammarCheck[i].correction}!)`).catch(console.error);
+      })
       message.channel.send(`Woof woof! (Hey, did you know that it's actually spelt *"${grammarCheck[i].correction}"*? Have a nice day!)`);
     }
   }
@@ -112,7 +112,7 @@ client.on('messageUpdate', (oldMessage, newMessage) => {
   const swearingEnabled = require('./commands/noswearing.js')
   if (!swearingEnabled) {
     const swearWords = require('./commands/data/swearwords.js');
-    const removeDupChar = s => s.split("").reduce((a, b) => (a[a.length - 1] != b) ? (a + b) : a, "");
+    const removeDupChar = s => s.split("").reduce((a, b) => (a[a.length - 1] !== b) ? (a + b) : a, "");
     const alphanumericOnlyMsg = newMessage.content.toLowerCase().replace(/[^a-z0-9$]/g, "").replace(/[$]/g, "s");
     const noDupCharMsg = removeDupChar(alphanumericOnlyMsg);
     if (swearWords.some(substring => noDupCharMsg.includes(substring)) || swearWords.some(substring => alphanumericOnlyMsg.includes(substring))) {
@@ -122,26 +122,57 @@ client.on('messageUpdate', (oldMessage, newMessage) => {
   }
 });
 
+client.on('messageDelete', async message => {
+  if (!message.guild) return;
+
+	const fetchedLogs = await message.guild.fetchAuditLogs({
+		limit: 1,
+		type: 'MESSAGE_DELETE',
+	});
+	const deletionLog = fetchedLogs.entries.first();
+
+	if (!deletionLog) return console.log(`A message by ${message.author.tag} was deleted, but no relevant audit logs were found.`);
+
+	const { executor } = deletionLog;
+  const channel = message.guild.channels.cache.find(ch => ch.name === 'server-logs');
+
+  if (message.content === '') {
+    message.content = "Message only contained the below attachment.";
+  }
+
+  if (executor.id === message.author.id) {
+    channel.send(`**${new Date().toLocaleString('en-US')}**: A message from ${message.author.username} was deleted either by themselves or by a bot:\n> ${message}`);
+  } else {
+    channel.send(`**${new Date().toLocaleString('en-US')}**: A message from ${message.author.username} was deleted by ${executor.username}:\n> ${message}`);
+  }
+
+  if (message.attachments.size > 0) {
+    channel.send(message.attachments.first().proxyURL)
+  }
+});
+
 client.on('guildMemberAdd', member => {
   const channel = member.guild.channels.cache.find(ch => ch.name === 'server-logs');
-  channel.send(`${new Date().toLocaleString('en-US')}: ${member} has joined the server.`);
+  channel.send(`**${new Date().toLocaleString('en-US')}**: ${member} has joined the server.`);
   if (member.id !== 186602980563222528) { // If new member is not Gordia
     member.roles.set(['559938493518970902']).catch(console.error)
     if (member.id == 326520499100319745) { // Ian
-      member.roles.set(['562002042512474132', '559933178152747049', '689084435785711666']).catch(console.error)
+      member.roles.set(['562002042512474132', '326520499100319745', '559938493518970902', '559933178152747049', '689084435785711666']).catch(console.error)
     }
   }
 });
 
 client.on('guildMemberUpdate', (oldMember, newMember) => {
   if (oldMember.id == 326520499100319745 && oldMember.roles.cache.size > newMember.roles.cache.size) { // Ian
-    newMember.roles.set(['562002042512474132', '559938493518970902', '559933178152747049', '689084435785711666']).catch(console.error)
+    newMember.roles.set(['562002042512474132', '326520499100319745', '559938493518970902', '559933178152747049', '689084435785711666']).catch(console.error)
   }
 });
 
 client.on('guildMemberRemove', member => {
   const channel = member.guild.channels.cache.find(ch => ch.name === 'server-logs');
-  channel.send(`${new Date().toLocaleString('en-US')}: ${member} has left the server.`);
+  let listOfRoles = []
+  member.roles.cache.each(role => listOfRoles.push(role.name.replace('@', '')))
+  channel.send(`**${new Date().toLocaleString('en-US')}**: ${member} has left the server.\nTheir roles were: ${listOfRoles.join(", ")}`);
 });
 
 client.login(token);
